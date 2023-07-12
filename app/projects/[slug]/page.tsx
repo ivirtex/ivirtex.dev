daa0faf17cdd2c6339ef73c16ac9783b0a8868a3
ivirtex <hjozwiakdx@gmail.com>
2023-07-12 11:20:38 +0200

    Use logo instead of back button

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,10 +1,11 @@
 import Footer from "@/app/components/footer";
+import Logo from "@/app/components/logo";
 import { Project, allProjects } from "contentlayer/generated";
 import "highlight.js/styles/github-dark.css";
 import { getMDXComponent } from "next-contentlayer/hooks";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { FaArrowLeft, FaGithub } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
 
 function ProjectNavigation(project: Project) {
   const className = "hover:text-neutral-300 duration-150 transition";
@@ -12,8 +13,12 @@ function ProjectNavigation(project: Project) {
   return (
     <header className="sticky top-0 mb-16 flex h-16 w-full items-center justify-around bg-neutral-950/50 backdrop-blur-md backdrop-saturate-150">
       <nav className="flex w-full max-w-5xl justify-between px-4 text-xl transition-all duration-500">
-        <Link href="/projects" className={className}>
-          <FaArrowLeft size={26} />
+        <Link
+          href="/"
+          className={`${className} font-semibold`}
+          aria-label="Home"
+        >
+          <Logo className="animate-svg h-7 w-auto fill-white" />
         </Link>
         <div className="space-x-4">
           <a href={project.repositoryUrl} className={className}>
